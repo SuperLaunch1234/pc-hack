ad8cdbb59abd96dc2197b6286477e10f2e997d03
SuperLaunch1234 <152689349+SuperLaunch1234@users.noreply.github.com>
2024-06-28 20:06:28 +1200

    Update and rename download.cmd to readme.cmd

--- installer/download.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo You Are About To Install A App Designed As A Prank
-set /p DUMMY=Hit ENTER to continue...
-https://github.com/SuperLaunch1234/pc-hack/tree/main/Scripts
---
+++ installer/readme.cmd
@@ -0,0 +1,7 @@
+color 3
+@echo off
+echo Here Is Some Files You Need To Make For This To Work
+echo 1. Make A Folder Called virusdev in the C:/ Drive
+echo 2. Move All Files Into That Folder Besides The File Called main.cmd
+echo 3. DO NOT EVER REMOVE THE FILES FROM THE FOLDERS WHEN YOU PLACE THEM INSIDE THE virusdev FOLDER
+echo 4. Only Download Files From https://github.com/SuperLaunch1234\pc-hack.git
