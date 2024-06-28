f450ade60e192710ae7883370a1735485534b15c
SuperLaunch1234 <152689349+SuperLaunch1234@users.noreply.github.com>
2024-06-28 19:54:29 +1200

    Update download.cmd

--- installer/download.cmd
+++ installer/download.cmd
@@ -1,3 +1,4 @@
+@echo off
 echo You Are About To Install A App Designed As A Prank
-echo --------------
 set /p DUMMY=Hit ENTER to continue...
+https://github.com/SuperLaunch1234/pc-hack/tree/main/Scripts
