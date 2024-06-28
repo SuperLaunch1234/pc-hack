dc9f7b5d6a48ad5b8ad61615128463c081d52f90
SuperLaunch1234 <152689349+SuperLaunch1234@users.noreply.github.com>
2024-06-28 20:10:52 +1200

    Update readme.cmd

--- installer/readme.cmd
+++ installer/readme.cmd
@@ -5,4 +5,4 @@ echo 1. Make A Folder Called virusdev in the C:/ Drive
 echo 2. Move All Files Into That Folder Besides The File Called main.cmd
 echo 3. DO NOT EVER REMOVE THE FILES FROM THE FOLDERS WHEN YOU PLACE THEM INSIDE THE virusdev FOLDER
 echo 4. Only Download Files From https://github.com/SuperLaunch1234\pc-hack.git
-pause
+set /p input=
